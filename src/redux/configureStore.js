fc3e716513781bdd6a6949324fd1909226f0f2c4
joel <joelkalema63@gmail.com>
2022-05-03 14:18:38 +0200

    Fixed linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-import booksReducer from './Books/books';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
   booksReducer,
